0b80d1213de7eb49705c018656889fa38632ebdb
Nicholas Mario Wardhana <wardhanan@cardiff.ac.uk>
2018-02-20 20:34:17 +0000

    Some further documentation in Topology

--- TopoLogicDynamo/include/Topology.h
+++ TopoLogicDynamo/include/Topology.h
@@ -14,50 +14,58 @@ namespace TopoLogic
 	public ref class Topology abstract
 	{
 	public:
+		/// <summary>
+		/// Returns the dimensionality of the topological entity.
+		/// </summary>
+		/// <returns name="int">The topological entity's dimentionality</returns>
 		property int Dimensionality
 		{
-			/// <summary>
-			/// 
-			/// </summary>
-			/// <returns name="int">The shape's dimentionality</returns>
 			int get();
 		}
 
 		/// <summary>
-		/// 
+		/// Creates the host geometry counterpart of the topological entity. The mapping is as follows. 
+		/// - Vertex: Point;
+		/// - Edge: either a Curve or its subclasses, other than Polycurve or its subclasses; 
+		/// - Wire: PolyCurve or Polygon; 
+		/// - Face: Surface or NurbsSurface;
+		/// - Shell: PolySurface; 
+		/// - Cell: either a Solid or its subclasses; 
+		/// - CellComplex: a list of Solids; 
+		/// - Cluster: a list of topological entities without a constituent parent.
 		/// </summary>
-		/// <returns name="Geometry">The host geometry counter part of the topological shape</returns>
+		/// <returns name="Geometry">The host geometry counterpart of the topological entity</returns>
 		property Object^ Geometry
 		{
 			virtual Object^ get() abstract;
 		}
 
 		/// <summary>
-		/// 
+		/// Returns the locked flag of this topological entity
 		/// </summary>
-		/// <returns name="bool">The locked flag of this shape</returns>
+		/// <returns name="bool">The locked flag of this topological entity</returns>
 		property bool Locked
 		{
 			bool get();
 		}
 
 		/// <summary>
-		/// 
+		/// Set the topology's locked flag
 		/// </summary>
 		/// <param name="value"></param>
 		/// <returns name="Topology">The original topology with the new locked flag</returns>
 		Topology^ SetLocked(bool value);
 
 		/// <summary>
-		/// 
+		/// Returns the topological entities containing the input topology as a non-constituent member
 		/// </summary>
-		/// <returns name="Topology[]">The topological shapes containing the input shape as a non-constituent member.</returns>
+		/// <returns name="Topology[]">The topological entities containing the input topology as a non-constituent member</returns>
 		List<Topology^>^ MemberOf();
 
 		/// <summary>
-		/// 
+		/// Returns the non-constituent members of the input topological entity.
 		/// </summary>
-		/// <returns name="Topology[]">The non-constituent members of the input topological shape.</returns>
+		/// <returns name="Topology[]">The non-constituent members of the input topological entity</returns>
 		List<Topology^>^ Members();
 
 		/// <summary>
@@ -78,80 +86,80 @@ namespace TopoLogic
 		Dictionary<String^, Object^>^ BooleanImages(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Perform the Difference operation between the first and second topological entities.
 		/// </summary>
-		/// <param name="topology"></param>
+		/// <param name="topology">The second topological entity</param>
 		/// <returns name="Topology">The result of the Difference operation</returns>
 		Topology^ Difference(Topology^ topology);
 		
 		/// <summary>
-		/// Impose the second topology to the first one.
+		/// Impose the second topological entity to the first one.
 		/// </summary>
 		/// <param name="topology">Another topology</param>
 		/// <returns name="Topology">The result of the Impose operation</returns>
 		Topology^ Impose(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Imprint the second topological entity to the first one.
 		/// </summary>
-		/// <param name="topology"></param>
+		/// <param name="topology">Another topology</param>
 		/// <returns name="Topology">The result of the Imprint operation</returns>
 		Topology^ Imprint(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Perform the Intersection operation between the first and the second topological entities.
 		/// </summary>
 		/// <param name="topology"></param>
 		/// <returns name="Topology">The result of the Intersection operation</returns>
 		Topology^ Intersection(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Merge the first and the second topological entities.
 		/// </summary>
 		/// <param name="topology"></param>
 		/// <returns name="Topology">The result of the Merge operation</returns>
 		Topology^ Merge(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Slice the first topological entity with the second.
 		/// </summary>
 		/// <param name="topology"></param>
 		/// <returns name="Topology">The result of the Slice operation</returns>
 		Topology^ Slice(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Union the first and the second topological entities.
 		/// </summary>
 		/// <param name="topology"></param>
 		/// <returns name="Topology">The result of the Union operation</returns>
 		Topology^ Union(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Perform the XOR operation between the first and the second topological entities.
 		/// </summary>
 		/// <param name="topology"></param>
 		/// <returns name="Topology">The result of the XOR operation</returns>
 		Topology^ XOR(Topology^ topology);
 
 		/// <summary>
-		/// 
+		/// Save a topological entity to a BRep file.
 		/// </summary>
-		/// <param name="path"></param>
+		/// <param name="path">The path to the BRep file</param>
 		/// <returns name="bool">True if the BRep file is succesffully created, otherwise false</returns>
 		bool SaveToBRep(String^ path);
 
 		/// <summary>
-		/// 
+		/// Load a topological entity from a BRep file.
 		/// </summary>
-		/// <param name="path"></param>
+		/// <param name="path">The path to the BRep file</param>
 		/// <returns name="bool">True if the BRep file is succesffully loaded, otherwise false</returns>
 		bool LoadFromBRep(String^ path);
 
 		/// <summary>
-		/// 
+		/// Print the topological information of the input entity.
 		/// </summary>
 		/// <param name="topology"></param>
-		/// <returns name="String">The topological information of the input shape</returns>
+		/// <returns name="String">The topological information of the input entity</returns>
 		String^ Analyze();
 
 	public protected:
