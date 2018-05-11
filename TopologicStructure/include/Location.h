b8085eec8879170c3f6201244b36f35ec2ba11c1
Nicholas Mario Wardhana <wardhanan@cardiff.ac.uk>
2018-05-11 16:18:51 +0100

    TopologicStructure

---
+++ TopologicStructure/include/Location.h
@@ -0,0 +1,17 @@
+#pragma once
+
+using namespace TopoLogic;
+
+namespace TopologicStructure
+{
+	public ref class Location
+	{
+	public:
+
+	public protected:
+
+	protected:
+		Location();
+		~Location();
+	};
+}
---
+++ TopologicStructure/src/Location.cpp
@@ -0,0 +1,14 @@
+#include <Location.h>
+
+
+namespace TopologicStructure
+{
+	Location::Location()
+	{
+		throw gcnew System::NotImplementedException();
+	}
+	Location::~Location()
+	{
+		throw gcnew System::NotImplementedException();
+	}
+}
