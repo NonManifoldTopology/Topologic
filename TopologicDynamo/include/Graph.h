c865870add53563683302af5fed6db54fdf07974
c1032831 <ChatzivasileiadiA@cardiff.ac.uk>
2019-01-17 12:26:16 +0000

    Descriptions added for DualGraph class.

--- TopologicDynamo/include/Graph.h
+++ TopologicDynamo/include/Graph.h
@@ -8,31 +8,30 @@ namespace Topologic
 {
 	ref class CellComplex;
 
-	namespace Extensions
 	/// <summary>
-	/// Extension currently includes the Graph class.
+	/// Extension currently includes the DualGraph class.
 	/// </summary>
 
+	namespace Extensions
+
+
 	{
-		public ref class DualGraph_ : Cluster
 		/// <summary>
 		/// A Graph consists of a set of vertices and a set of edges that connect the vertices. It can be a Wire, Vertex, Edge or Cluster and is defined by the input Topology. It can be manifold or non-manifold.
 		/// </summary>
+		public ref class DualGraph_ : Cluster
+		
 		{
 		public:
 			/// <summary>
-			/// Creates a dual graph by a CellComplex. Options to be selected for the creation of the dual graph is the use of cells, non-manifold faces, manifold faces and apertures.
+			/// Creates a dual graph by a CellComplex.
 			/// </summary>
-			/// <param name="cellComplex">The cell complex to be used for the creation of the dual graph</param>
-			/// <param name="direct"></param>
-			/// <param name="viaSharedFaces"></param>
-			/// <param name="viaSharedApertures"></param>
-			/// <param name="toExteriorFaces"></param>
-			/// <param name="toExteriorApertures"></param>
-			/// <param name="useCells">Choose whether to use the cells of the cell complex or not</param>
-			/// <param name="useNonManifoldFaces">Choose whether to use the non-manifold faces of the cell complex or not</param>
-			/// <param name="useManifoldFaces">Choose whether to use the manifold faces of the cell complex or not</param>
-			/// <param name="useApertures">Choose whether to use the apertures of the cell complex or not</param>
+			/// <param name="cellComplex">The CellComplex to be used for the creation of the dual graph</param>
+			/// <param name="direct">Choose whether to use the centroids of the Cells in the CellComplex or not</param>
+			/// <param name="viaSharedFaces">Choose whether to use the shared Faces in the CellComplex or not</param>
+			/// <param name="viaSharedApertures">Choose whether to use any shared Apertures in the CellComplex or not</param>
+			/// <param name="toExteriorFaces">Choose whether to use the exterior Faces of the CellComplex or not</param>
+			/// <param name="toExteriorApertures">Choose whether to use any exterior Apertures of the CellComplex or not</param>
 			/// <returns>The created dual graph</returns>
 			static DualGraph_^ ByCellComplex(
 				CellComplex^ cellComplex,
@@ -43,15 +42,15 @@ namespace Topologic
 				[DefaultArgument("false")] bool toExteriorApertures);
 
 			/// <summary>
-			/// 
+			/// Creates a dual graph by a Shell.
 			/// </summary>
-			/// <param name="cellComplex"></param>
-			/// <param name="direct"></param>
-			/// <param name="viaSharedEdges"></param>
-			/// <param name="viaSharedApertures"></param>
-			/// <param name="toExteriorEdges"></param>
-			/// <param name="toExteriorApertures"></param>
-			/// <returns></returns>
+			/// <param name="shell">The Shell to be used for the creation of the dual graph</param>
+			/// <param name="direct">Choose whether to use the centroids of the Faces in the Shell or not</param>
+			/// <param name="viaSharedEdges">Choose whether to use the shared Edges in the Shell or not</param>
+			/// <param name="viaSharedApertures">Choose whether to use any shared Apertures in the Shell or not</param>
+			/// <param name="toExteriorEdges">Choose whether to use the exterior Edges of the Shell or not</param>
+			/// <param name="toExteriorApertures">Choose whether to use any exterior Apertures of the Shell or not</param>
+			/// <returns>The created dual graph</returns>
 			static DualGraph_^ ByShell(
 				Shell^ shell,
 				[DefaultArgument("true")] bool direct,
