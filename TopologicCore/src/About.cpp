2664b0196526abd8cbb27b71218873b37a551efc
Nicholas Mario Wardhana <wardhanan@cardiff.ac.uk>
2019-02-15 15:48:23 +0000

    Bugfix with glazing ratio
    Nullable glazing ratio

--- TopologicCore/src/About.cpp
+++ TopologicCore/src/About.cpp
@@ -2,12 +2,13 @@
 
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 8
-#define BUILD_VERSION 0
+#define EXTERNAL_BUILD_VERSION 0
+#define INTERNAL_BUILD_VERSION 1
 
 namespace TopologicCore
 {
 	std::string About::Version()
 	{
-		return std::to_string(MAJOR_VERSION) + "." + std::to_string(MINOR_VERSION) + "." + std::to_string(BUILD_VERSION);
+		return std::to_string(MAJOR_VERSION) + "." + std::to_string(MINOR_VERSION) + "." + std::to_string(EXTERNAL_BUILD_VERSION) + "." + std::to_string(INTERNAL_BUILD_VERSION);
 	}
 }
