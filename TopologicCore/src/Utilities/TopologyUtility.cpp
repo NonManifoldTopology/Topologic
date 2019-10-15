0315b3aa7555ef438f86c4801390015657f5b8fb
Nicholas Mario Wardhana <wardhanan@cardiff.ac.uk>
2019-10-15 12:20:05 +0100

    Add the license text, and updated the license header.

--- TopologicCore/src/Utilities/TopologyUtility.cpp
+++ TopologicCore/src/Utilities/TopologyUtility.cpp
@@ -1,17 +1,18 @@
 // This file is part of Topologic software library.
 // Copyright(C) 2019, Cardiff University and University College London
 //
-// This program is free software : you can redistribute it and/or modify it
-// under the terms of the GNU Affero General Public License version 3 (AGPL v3)
-// as published by the Free Software Foundation.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
 //
-// Please consult the file LICENSE.txt included in Topologic distribution
-// for complete text of the license and disclaimer of any warranty.
-// Alternatively, please see https://www.gnu.org/licenses/agpl-3.0.en.html.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <Utilities/TopologyUtility.h>
 
